80ec03b675072cd78af3ef743a901b583f30e73e
Mina Ashna <minaashna@gmail.com>
2024-06-17 09:13:18 +0330

    Big Bang

---
+++ TimeKeeper/TimeKeeper/TimeKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeKeeperApp.swift
+//  TimeKeeper
+//
+//  Created by Mina Ashna on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
