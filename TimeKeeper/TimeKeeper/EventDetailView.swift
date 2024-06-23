b9c491d05b224a8d1313f5ab3d91d1d8f9d5a033
Mina Ashna <minaashna@gmail.com>
2024-06-23 10:23:09 +0330

    - add Gauge

--- TimeKeeper/TimeKeeper/EventDetailView.swift
+++ TimeKeeper/TimeKeeper/EventDetailView.swift
@@ -17,24 +17,98 @@ struct EventDetailView: View {
     var body: some View {
         VStack {
             Text(event.title)
+                .font(.title)
             
-            Text(String(format: "%02d:%02d:%02d:%02d:%02d:%02d", offset.year, offset.month, offset.day, offset.hour, offset.minute, offset.second))
-                .contentTransition(.numericText(countsDown: true))
-
+            Spacer()
             
-//            Text(offset.year + offset.month + offset.day + offset.hour + offset.minute + offset.second, style: .timer)
+//            Text(String(format: "%02d:%02d:%02d:%02d:%02d:%02d", offset.year, offset.month, offset.day, offset.hour, offset.minute, offset.second))
 //                .contentTransition(.numericText(countsDown: true))
+      
             
-            
-            Button {
-                isPresentingEditView = true
-                editingEvent = event
-            } label: {
-                Text("Edit")
+            VStack(spacing: 0) {
+                HStack {
+                    Text(event.creationDate, style: .date)
+                    Spacer()
+                    Text(event.endDate, style: .date)
+                }
+                .padding()
+                
+                Gauge(
+                    value: totalSeconds() - secondsLeft(),
+                    in: 0...totalSeconds(),
+                    label: { },
+                    currentValueLabel: {
+                        HStack {
+                            if offset.year > 0 {
+                                VStack {
+                                    Text("Year")
+                                        .foregroundStyle(Color.gray)
+                                    Text(offset.year.description)
+                                        .font(.headline)
+                                }
+                            }
+                            if offset.month > 0 {
+                                VStack {
+                                    Text("Month")
+                                        .foregroundStyle(Color.gray)
+                                    Text(offset.month.description)
+                                        .font(.headline)
+                                }
+                            }
+                            if offset.day > 0 {
+                                VStack {
+                                    Text("Day")
+                                        .foregroundStyle(Color.gray)
+                                    Text(offset.day.description)
+                                        .font(.headline)
+                                }
+                            }
+                            if offset.hour > 0 {
+                                VStack {
+                                    Text("Hour")
+                                        .foregroundStyle(Color.gray)
+                                    Text(offset.hour.description)
+                                        .font(.headline)
+                                }
+                            }
+                            if offset.minute > 0 {
+                                VStack {
+                                    Text("Minute")
+                                        .foregroundStyle(Color.gray)
+                                    Text(offset.minute.description)
+                                        .font(.headline)
+                                }
+                            }
+                            if offset.second > 0 {
+                                VStack {
+                                    Text("Second")
+                                        .foregroundStyle(Color.gray)
+                                    Text(offset.second.description)
+                                        .font(.headline)
+                                        .contentTransition(.numericText(countsDown: true))
+                                }
+                            }
+                        }
+                        .padding()
+                    }
+                )
+                .padding()
             }
+            
+            Spacer()
         }
         .onReceive(timer) { _ in
             countdown()
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    isPresentingEditView = true
+                    editingEvent = event
+                } label: {
+                    Text("Edit")
+                }
+            }
         }
         .sheet(isPresented: $isPresentingEditView) {
             NavigationStack {
@@ -80,8 +154,19 @@ struct EventDetailView: View {
         offset.minute = diff.minute ?? 0
         offset.second = diff.second ?? 0
     }
+    
+    func secondsLeft() -> Double {
+        let diff = event.endDate.timeIntervalSince(.now)
+        return round(diff)
+    }
+    func totalSeconds() -> Double {
+        let diff = event.endDate.timeIntervalSince(event.creationDate)
+        return round(diff)
+    }
 }
 
 #Preview {
-    EventDetailView(event: Event.sampleEvents[0])
+    NavigationStack {
+        EventDetailView(event: Event.sampleEvents[0])
+    }
 }
